2acce6a8ffdb1dbb26ea378308ae273a24c0e492
Nik. Ioannoy <FamousKiller12@gmail.com>
2024-04-02 18:59:52 +0300

    Table Sorting

--- src/app/components/simple-datatable/simple-datatable.component.ts
+++ src/app/components/simple-datatable/simple-datatable.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { EPerson } from 'src/app/shared/interfaces/person';
+import { sortBy } from 'lodash-es';
+
 
 @Component({
   selector: 'app-simple-datatable',
@@ -9,6 +11,40 @@ import { EPerson } from 'src/app/shared/interfaces/person';
   styleUrl: './simple-datatable.component.css'
 })
 export class SimpleDatatableComponent {
-  @Input() data: EPerson[]
+  @Input() data: EPerson[] //data(onoma metavlitis) tha lavei dedomena EPerson[]
   
+  sortOrder = {
+    givenName: 'none',
+    surName: 'none',
+    age: 'none',
+    email: 'none',
+    education: 'none'
+  }
+
+  sortData(sortKey: string) {
+    if (this.sortOrder[sortKey] === 'asc') {
+      this.sortOrder[sortKey] = 'desc'
+      this.data = sortBy(this.data, sortKey).reverse() //data=pinakas, sortKey = pedio me to opoio epileksame na ginei sort, reverse= fora taksinomishs
+    } else {
+      this.sortOrder[sortKey] = 'asc'
+      this.data = sortBy(this.data, sortKey)
+    }
+    for(let key in this.sortOrder) {
+      if(key != sortKey) {
+        this.sortOrder[key] = 'none'
+        //ΑΝ ΔΕΝ ΕΧΩ ΠΑΤΗΣΕΙ ΑΥΤΟ ΤΟ KEY ΤΟ ΚΑΝΩ ΝΟΝΕ
+      }
+    }
+  }
+
+  sortSign(sortKey: string) {
+    if (this.sortOrder[sortKey] === 'asc') {
+      return '↑';
+    } else if (this.sortOrder[sortKey] === 'desc') {
+      return '↓';
+    } else {
+      return '';
+    }
+  }
 }
+
