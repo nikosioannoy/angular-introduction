18f5325131427894819394a2254a7b71b3a75fb7
Nik. Ioannoy <FamousKiller12@gmail.com>
2024-04-08 20:45:08 +0300

    Initial Commit

---
+++ src/app/components/eperson-reactive-form/eperson-reactive-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eperson-reactive-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './eperson-reactive-form.component.html',
+  styleUrl: './eperson-reactive-form.component.css'
+})
+export class EpersonReactiveFormComponent {
+
+}
