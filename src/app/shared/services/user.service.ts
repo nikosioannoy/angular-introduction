745608fb3b6c0f0ffdea018a88770698764c7624
Nik. Ioannoy <FamousKiller12@gmail.com>
2024-04-09 21:05:19 +0300

    initial commit

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
+import { User } from '../interfaces/user';
 
 const API_URL = `${environment.apiURL}/user`
 
@@ -12,4 +13,8 @@ const API_URL = `${environment.apiURL}/user`
 export class UserService {
 
   http: HttpClient = inject(HttpClient)
+
+  registerUser(user: User) {
+    return this.http.post<{msg: string }>(`${API_URL}/register`, user)
+  }
 }
